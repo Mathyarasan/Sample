404801a5658bb417db989b71c8dc15d74466a184
Mathyarasan <mathyaras1704@gmail.com>
2022-11-07 14:10:26 +0530

    initial commit

---
+++ Java_Concept/src/com/java/Simple.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Simple {
+
+	public static void run() {
+		// TODO Auto-generated method stub
+System.out.println("mathy");
+	}
+	public static void main(String[] args) {
+		run();
+	}
+}
